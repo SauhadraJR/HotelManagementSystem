3ab0e81720b3aecd84c091109b75ce1a1b1a5efe
Sauhadra Jung Rayamajhi <sauhadra.rayamajhi@gmail.com>
2024-02-21 07:50:28 +0545

    management views and serializers

--- HMS/settings.py
+++ HMS/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'management',
     'restaurant',
     'frontdesk',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- management/views.py
+++ management/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import RoomType, Room
+from .serializers import RoomTypeSerializer, RoomSerializer
 
 # Create your views here.
+class RoomTypeView(ModelViewSet):
+    queryset = RoomType.objects.all()
+    serializer_class = RoomTypeSerializer
+
+class RoomView(ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
